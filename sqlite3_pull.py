39fecb14df9b1905ea42a1b389fe743e02761e7e
zacharygibbs <zachary.m.gibbs@gmail.com>
2021-01-23 10:03:43 -0600

    fixed database calls so that data is pulling directly from sqlite database, limiting app memory requirements

--- sqlite3_pull.py
+++ sqlite3_pull.py
@@ -75,9 +75,23 @@ def state_counties(state):
     conn.close()
     return res
 
-def state_dropdown(state):
+def state_dropdown():
     conn = sqlite3.connect('covid_data.db',check_same_thread=False,timeout=3000)
-    res=pd.read_sql('SELECT state,cases FROM states'%(state),conn)
+    res=pd.read_sql("""
+                        SELECT * FROM states
+                        WHERE date=(SELECT max(date) from states)
+                        ORDER BY cases DESC;
+                        """,conn)
     conn.close()
-    return res['state'], res['cases']:
+    return list(zip(res['state'], res['cases']))
+
+def county_dropdown(state):
+    conn = sqlite3.connect('covid_data.db',check_same_thread=False,timeout=3000)
+    res=pd.read_sql("""
+                        SELECT * FROM counties
+                        WHERE date=(SELECT max(date) from counties) AND state="%s"
+                        ORDER BY cases DESC;
+                        """%(state),conn)
+    conn.close()
+    return list(zip(res['county'], res['cases']))
 
